de16391aadf96d44ffcf80c35648d5c09fbcbf46
Welcome <madhuribarabari@gmail.com>
2022-10-02 10:45:15 +0530

    Initial commit

---
+++ src/hellojava.java
@@ -0,0 +1,10 @@
+public class hellojava {
+    public static void main(String[] args) {
+        System.out.println("My Name Is Madhuri");
+        String name = "My Name Is Madhuri";
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+
+    }
+
+}
